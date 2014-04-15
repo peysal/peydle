e020dfdde90cec22a5fa75b5185f1374e6c3691d
peysal <peysal@gmail.com>
2014-04-15 18:17:30 +0800

    poc monitor file operation inside directory

---
+++ src/main/groovy/BackupListener.groovy
@@ -0,0 +1,24 @@
+package main.groovy
+
+import org.apache.commons.vfs2.FileChangeEvent
+import org.apache.commons.vfs2.FileListener
+
+/**
+ * Backup listener.
+ */
+class BackupListener implements FileListener {
+    @Override
+    void fileCreated(FileChangeEvent event) throws Exception {
+        println("file created")
+    }
+
+    @Override
+    void fileDeleted(FileChangeEvent event) throws Exception {
+        println "file deleted"
+    }
+
+    @Override
+    void fileChanged(FileChangeEvent event) throws Exception {
+        println "file changed"
+    }
+}
--- src/main/groovy/Preparation.groovy
+++ src/main/groovy/Preparation.groovy
@@ -1,18 +1,33 @@
 package main.groovy
 
+import org.apache.commons.vfs2.FileObject
+import org.apache.commons.vfs2.FileSystemManager
+import org.apache.commons.vfs2.VFS
+import org.apache.commons.vfs2.impl.DefaultFileMonitor
+
 /**
  * Prepare the environment for the testing to happen in Scenario phase.
  * 1) it assume the orchestra tool already running successfully
  * 2) it assume the business test case folder convention is setup successfully
  * 3) it assume the file exist
  */
+//
+//Utils.print("+", "starting preparation")
+//
+//def businessTestNumber = args[0]
+//Utils.info("+", "businessTestNumber=${businessTestNumber}")
 
-Utils.print("+", "starting preparation")
+FileSystemManager manager = VFS.getManager();
+def absoluteLocation = new File('D:/learningWorkspace/peydle/executionClient/backup')
+println absoluteLocation.absolutePath
+FileObject file= manager.resolveFile(absoluteLocation.absolutePath);
 
-def businessTestNumber = args[0]
-Utils.info("+", "businessTestNumber=${businessTestNumber}")
-
-def btFolder = new File()
-
-def ant = new AntBuilder()
+DefaultFileMonitor fm = new DefaultFileMonitor(new BackupListener());
+fm.setRecursive(true);
+fm.addFile(file);
+fm.start()
+for (int i = 1; i < 6; i++) {
+    println "sleeps for 5 second at step ${i}"
+    sleep(5000)
+}
 
