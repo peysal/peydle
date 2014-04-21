078d5280b732ffa975dc6ec3005934609c50de16
peysal <peysal@gmail.com>
2014-04-21 14:26:27 +0800

    Generate tenants.properties content in batch

---
+++ src/main/groovy/TenantsGenerator.groovy
@@ -0,0 +1,22 @@
+package main.groovy
+
+import groovy.text.GStringTemplateEngine
+
+def f = new File('d:/learningWorkspace/peydle/src/main/resources/tenants.template')
+engine = new GStringTemplateEngine()
+
+def usersClientInfo = [[user: "User 1", clients:["A", "B"]], [user: "User 2", clients:["C", "D"]],
+        [user: "User 3", clients:["E", "F"]], [user: "User 4", clients:["G", "H"]], [user: "User 5", clients:["I", "J"]],
+        [user: "Explorer 1", clients:["X"]], [user: "Explorer 2", clients:["Y"]], [user: "Explorer 3", clients:["Z"]]]
+
+def templatesOutput = ""
+
+usersClientInfo.each { userClientInfo ->
+    userClientInfo.clients.each { client ->
+        def binding = ["user": userClientInfo.user, "client": client]
+        template = engine.createTemplate(f).make(binding)
+        templatesOutput += (template.toString() + "\n\n")
+    }
+}
+
+println templatesOutput
