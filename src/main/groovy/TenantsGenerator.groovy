454ea4e677c6740a205f6ebe8a71d3eafcbe9825
peysal <peysal@gmail.com>
2014-04-21 17:09:39 +0800

    custom setting for tenants

--- src/main/groovy/TenantsGenerator.groovy
+++ src/main/groovy/TenantsGenerator.groovy
@@ -2,12 +2,10 @@ package main.groovy
 
 import groovy.text.GStringTemplateEngine
 
-def f = new File('d:/learningWorkspace/peydle/src/main/resources/tenants.template')
+def f = new File('babun/tenants.template')
 engine = new GStringTemplateEngine()
 
-def usersClientInfo = [[user: "User 1", clients:["A", "B"]], [user: "User 2", clients:["C", "D"]],
-        [user: "User 3", clients:["E", "F"]], [user: "User 4", clients:["G", "H"]], [user: "User 5", clients:["I", "J"]],
-        [user: "Explorer 1", clients:["X"]], [user: "Explorer 2", clients:["Y"]], [user: "Explorer 3", clients:["Z"]]]
+def usersClientInfo = [[user: "User 1", clients:["A", "B"]], [user: "User 2", clients:["C", "D"]]]
 
 def templatesOutput = ""
 
