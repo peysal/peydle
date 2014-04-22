28a4ec989ed3be523c9790c20ed1087674b70d1e
peysal <peysal@gmail.com>
2014-04-22 17:08:25 +0800

    fast exit if preparation step is broken

--- src/main/groovy/E2e.groovy
+++ src/main/groovy/E2e.groovy
@@ -13,6 +13,9 @@ def backupAbsoluteLocation = args[1]
 def incomingAbsoluteLocation = args[2]
 def secondsToSleep = args[3] as Long
 def timeSplitter = 2
+def isPreparationIncorrect = false
+def inputFiles = []
+
 Utils.info("+", "btPath=${btPath}, " +
         "backupAbsoluteLocation=${backupAbsoluteLocation}, \n" +
         "incomingAbsoluteLocation=${incomingAbsoluteLocation}")
@@ -21,7 +24,7 @@ def ant = new AntBuilder()
 
 new File(btPath).eachFile { file ->
     println "move ${file.name} to incoming folder"
-    ant.move(file: file, toDir: backupAbsoluteLocation)
+    //ant.move(file: file, toDir: incomingAbsoluteLocation)
     println "now looking for ${file.name} inside backup for ${secondsToSleep/1000} seconds"
     long secondsPerProcess = secondsToSleep / timeSplitter
     def secondsLeft = secondsToSleep
@@ -35,7 +38,14 @@ new File(btPath).eachFile { file ->
         if (fileProcessed.exists()) {
             println "file ${fileProcessed.name} found!!"
             secondsLeft = 0
+        } else if (secondsLeft <= 0) {
+            isPreparationIncorrect = true
+            println "!!whole process is screw up because ${file.name} not process fast enough by orchestra!!"
         }
-
     }
+    if (isPreparationIncorrect)
+        System.exit(1)
+    inputFiles << file
 }
+
+
