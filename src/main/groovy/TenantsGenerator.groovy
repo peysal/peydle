220ef7f737420f6ac0e999fe4bd3f168b6fb1890
peysal <peysal@gmail.com>
2014-04-22 10:58:49 +0800

    environment property

--- src/main/groovy/TenantsGenerator.groovy
+++ src/main/groovy/TenantsGenerator.groovy
@@ -2,10 +2,12 @@ package main.groovy
 
 import groovy.text.GStringTemplateEngine
 
-def f = new File('babun/tenants.template')
+def f = new File('d:/learningWorkspace/peydle/src/main/resources/tenants.template')
 engine = new GStringTemplateEngine()
 
-def usersClientInfo = [[user: "User 1", clients:["A", "B"]], [user: "User 2", clients:["C", "D"]]]
+def usersClientInfo = [[user: "User 1", clients:["A", "B"]], [user: "User 2", clients:["C", "D"]],
+        [user: "User 3", clients:["E", "F"]], [user: "User 4", clients:["G", "H"]], [user: "User 5", clients:["I", "J"]],
+        [user: "Explorer 1", clients:["X"]], [user: "Explorer 2", clients:["Y"]], [user: "Explorer 3", clients:["Z"]]]
 
 def templatesOutput = ""
 
